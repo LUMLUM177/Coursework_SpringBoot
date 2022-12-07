d5349e71042ecfa7ef410acc2b83db3caed54096
LUM <lumlum177@gmail.com>
2022-12-07 08:29:09 +0300

    Началный коммит

---
+++ src/main/java/coursework_springboot/CourseworkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package coursework_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coursework_springboot/CourseworkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package coursework_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
